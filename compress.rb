d408ac59e85040f6824d2ea271cbde80cde148bb
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2018-01-22 17:25:07 -0500

    reorganize base functions

--- compress.rb
+++ compress.rb
@@ -1,21 +1,2 @@
 require 'zlib'
 
-def to_base(n, base)
-    res = []
-    while n != 0
-        n, e = n.divmod base
-        res.unshift e
-    end
-    res
-end
-
-def from_base(n, base)
-    n = n.clone
-    pow = 1
-    res = 0
-    until n.empty?
-        res += n.pop * pow
-        pow *= base
-    end
-    res
-end
--- lib.rb
+++ lib.rb
@@ -666,3 +666,23 @@ def positions(arr, els)
         [k, arr.indices(k)]
     }
 end
+
+def to_base(n, base)
+    res = []
+    while n != 0
+        n, e = n.divmod base
+        res.unshift e
+    end
+    res
+end
+
+def from_base(n, base)
+    n = n.clone
+    pow = 1
+    res = 0
+    until n.empty?
+        res += n.pop * pow
+        pow *= base
+    end
+    res
+end
