386d96ff946769d956e208f1e9785ecb561ac264
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2018-03-09 20:15:24 +0000

    begin expanding metattache

--- libs/metattache.rb
+++ libs/metattache.rb
@@ -8,3 +8,32 @@ AtState.function("Tokenize") { |inst, text|
         Token.new a, b.to_s, c
     }
 }
+
+AtState.function("Shunt") { |inst, text|
+    parse(text).map { |tok|
+        a, b, c = tok
+        Token.new a, b.to_s, c
+    }
+}
+
+def at_ast(node)
+    node = ast(node) unless Array === node
+    node
+end
+
+AtState.function("AST") { |inst, text|
+    at_ast(text)
+}
+
+AtState.function("Traverse", configurable: true) { |inst, nodes, **opts|
+    rec = lambda { |inst, node|
+        args = node.children.map { |child|
+            opts[:getValue][inst, child]
+        }
+        opts[:operators][node.head.raw][inst, *args]
+    }
+    
+    nodes.map { |child|
+        rec[inst, child]
+    }
+}
